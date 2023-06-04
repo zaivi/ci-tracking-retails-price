34bd4bcab87bc9652f2cb7d48d559868217520e1
zaivi <phanvigiaii@email.com>
2023-06-04 12:05:02 +0700

    main function

---
+++ main.py
@@ -0,0 +1,161 @@
+import re
+import os
+import sys
+import time
+import yaml
+import glob
+import datetime
+import requests
+import pandas as pd
+
+from utils import read_config, is_same_row, ensure_dir, parse_shopee_url, update_master_file
+
+
+def fetch_data(itemid, shopid):
+    """
+    Fetch data from shopee.vn api and return json data
+    API: https://shopee.vn/api/v4/item/get?itemid={itemid}&shopid={shopid}
+    Raise exception if response status code is not 200
+    """
+    url = f'https://shopee.vn/api/v4/item/get?itemid={itemid}&shopid={shopid}'
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
+    }
+    
+    response = requests.get(url, headers=headers)
+
+    if response.status_code != 200:
+        raise Exception(f'Failed to fetch data from {url}')
+
+    data = response.json().get('data')
+    if not data:
+        raise Exception(f'Failed to fetch data from {url}')
+
+    required_fieds = [
+        'itemid', 'shopid', 'name', 'description', 'price',
+        'price_before_discount', 'stock', 'sold', 'item_status', 'image',
+        'cmt_count', 'liked_count', 'shop_location'
+    ]
+
+    # validate data must have required fields
+    for field in required_fieds:
+        if field not in data:
+            raise Exception(f'Invalid data from {url}, missing {field}')
+
+    return data
+
+
+def update_db(data):
+    """
+    Store update data info to ./data/info/{itemid}.yaml file in yaml format
+    And append history  data to ./data/history/{itemid}.csv file
+    Only append new row if new data is not same as last row or if file not exist
+
+    File format {itemid}.yaml:
+
+    ```yaml
+    itemid: 5873954476
+    shopid: 88201679
+    name: Apple MacBook Air (2020) M1 Chip 13.3 inch 8GB 256GB SSD
+    image: http://...
+    description: ...
+    shop_location: Ho Chi Minh City
+    updated_at: 2020-05-01T00:00:00+07:00
+    ```
+
+    File format {itemid}.csv:
+
+    ```csv
+    time,date,itemid,price,discount,price_before_discount,stock,sold,item_status,cmt_count,liked_count
+    1610003537,20200101,5873954476,1000000,0,1000000,100,0,normal,100,100
+    1638204525,20200102,1587395447,6000000,0,1000000,99,1,normal,100,100
+    1638204553,20200102,1587395447,6000000,0,1000000,98,2,normal,100,100
+    ````
+    """
+    current_time = int(time.time())
+    current_date = time.strftime('%Y%m%d', time.localtime(current_time))
+    itemid = data.get('itemid')
+    shopid = data.get('shopid')
+    data['date'] = current_date
+
+    # create ./data/info and ./data/history directory if not exist
+    ensure_dir('./data/info')
+    ensure_dir('./data/history')
+
+    # update history file or create new file if not exist
+    history_file = f'./data/history/{itemid}.csv'
+    if os.path.exists(history_file):
+        df = pd.read_csv(history_file)
+    else:
+        df = pd.DataFrame(columns=[
+            'time', 'date', 'itemid', 'price', 'discount',
+            'price_before_discount', 'stock', 'sold', 'item_status',
+            'cmt_count', 'liked_count'
+        ])
+
+    # get the last rows of history file and convert to dict
+    last_row = df.iloc[-1].to_dict() if len(df) > 0 else None
+    if last_row and is_same_row(last_row, data, ignores=['time', 'itemid']):
+        print(f'No update for {itemid}')
+        return
+
+    # append new row
+    update_rows = {
+            'time': current_time,
+            'date': current_date,
+            'itemid': itemid,
+            'price': data.get('price'),
+            'discount': data.get('discount'),
+            'price_before_discount': data.get('price_before_discount'),
+            'stock': data.get('stock'),
+            'sold': data.get('sold'),
+            'item_status': data.get('item_status'),
+            'cmt_count': data.get('cmt_count'),
+            'liked_count': data.get('liked_count')
+        }
+    new_update_df = pd.DataFrame(update_rows, index=[0])
+    df = pd.concat([df, new_update_df])
+    df.to_csv(history_file, index=False)
+    print(f'Updated {history_file}')
+
+    info_data = {
+        'itemid': itemid,
+        'shopid': shopid,
+        'name': data.get('name'),
+        'image': data.get('image'),
+        'shop_location': data.get('shop_location'),
+        'updated_at': datetime.datetime.now().isoformat()
+    }
+    # create or update info file
+    info_file = f'./data/info/{itemid}.yaml'
+    with open(info_file, 'w') as f:
+        yaml.dump(info_data, f)
+        print(f'Updated {info_file}')
+
+
+def main():
+    """
+    Main function
+    """
+    # Reading configs
+    config = read_config('config.yaml')
+    urls = config.get('urls', [])
+    print(f'Got {len(urls)} from config file')
+
+    # Fetch data from shopee.vn api and update db
+    for url in urls:
+        try:
+            print(f'Processing: {url}')
+            itemid, shopid = parse_shopee_url(url)
+            data = fetch_data(itemid, shopid)
+            update_db(data)
+        except Exception as e:
+            print(f'Failed to process {url}')
+            print(e)
+
+    # This demontrate how to transform data and write to master file (output)
+    update_master_file()
+
+
+if __name__ == '__main__':
+    main()
---
+++ utils/load.py
@@ -0,0 +1,53 @@
+import os
+import yaml
+import glob
+import pandas as pd
+
+from .transformation import transform_price
+from .utils import get_item_name
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+root_dir = os.path.join(current_dir, '..')
+
+
+def update_master_file(src='./data/history/*.csv', target='./data/master.csv'):
+    """
+    Concat all history file to master file
+    Add the item name by lookup into ./data/info/{itemid}.yaml
+    """
+    print('Updating master file')
+
+    src_files = glob.glob(os.path.join(root_dir, src))
+
+    # check src files
+    if not src_files:
+        print('No history file found')
+        return
+
+    frames = []
+    for file in src_files:
+        frames.append(pd.read_csv(file))
+        
+    df = pd.concat(frames)
+
+    # sort by itemid and time desc
+    df = df.sort_values(by=['itemid', 'time'], ascending=[True, False])
+
+    # add item_name column
+    df['item_name'] = df['itemid'].apply(get_item_name)
+
+    # format price, price_before_discount
+    df['price'] = df['price'].apply(transform_price)
+    df['price_before_discount'] = df['price_before_discount'].apply(
+        transform_price)
+
+    # Add column is_latest to indicate newest record for each itemid
+    df['is_latest'] = df.groupby('itemid').cumcount() == 0
+
+    # save to master file
+    df.to_csv(target, index=False)
+
+    # Get file size of master file in KB
+    size = os.path.getsize(target) / 1024
+
+    print(f'Master file updated, size: {size:.2f} KB')
